69b6750b5bfd27b09a245120608d583977e728c9
bplc904 <adrian.dudek@consultant.volvo.com>
2018-03-28 11:35:06 +0200

    Application schema

---
+++ src/main/java/com/volvo/jvs/mqtest/MqTestApplication.java
@@ -0,0 +1,12 @@
+package com.volvo.jvs.mqtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqTestApplication.class, args);
+	}
+}
